1179ee983d3de4f8a03ff835904112816659887d
WaterChick <wolfsfromhell@gmail.com>
2025-01-16 18:18:57 +0100

    add: http client support

--- src/main/java/dev/waterchick/License.java
+++ src/main/java/dev/waterchick/License.java
@@ -1,13 +1,10 @@
 package dev.waterchick;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
 
 public class License {
@@ -25,19 +22,28 @@ public class License {
         if (Thread.currentThread().getName().equals("main")) {
             throw new IllegalStateException("This method can only be called asynchronously.");
         }
+
+        // JSON payload
         String jsonInputString = String.format("{\"licenseId\": \"%s\", \"pluginId\": \"%s\"}", licenseId, pluginId);
 
         try {
+            // Vytvoření HttpClientu
             HttpClient client = HttpClient.newHttpClient();
+
+            // Vytvoření požadavku
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(LICENSE_HOST))
-                    .header("Content-Type", "application/json")
+                    .header("Content-Type", "application/json; utf-8")
                     .header("Accept", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(jsonInputString))
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonInputString, StandardCharsets.UTF_8))
                     .build();
 
+            // Odeslání požadavku a zpracování odpovědi
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Kontrola odpovědi
             return response.statusCode() == 200;
+
         } catch (Exception e) {
             e.printStackTrace();
             return false;
